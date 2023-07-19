444add7e40dbecd55893b3b1ede54a843addafdb
Casporo <casper.g.kaun@gmail.com>
2023-07-19 17:03:00 +0800

    - Added scenario to test for selector - Added additional test files

--- example_casper/test-data-file/index.js
+++ example_casper/test-data-file/index.js
@@ -2,19 +2,24 @@ import { Selector } from 'testcafe';
 import XPathSelector from '../xpath-selector';
 
 const dataSet = require('./data.json');
+const firstCheckbox  = XPathSelector('//input[@type="checkbox"]');
+const developerName = XPathSelector('//*[@id="developer-name"]');
+const interfaceSelector = XPathSelector('//*[@id="preferred-interface"]');
+const interFaceOptions = interfaceSelector.find('option');
 
 fixture `Data-Driven Tests`
     .page `https://devexpress.github.io/testcafe/example/`;
 
 dataSet.forEach(data => {
     test(`Enter '${data.name}'`, async t => {
-    const firstCheckbox  = XPathSelector('//input[@type="checkbox"]');
-    const developerName = XPathSelector('//*[@id="developer-name"]');
+
         await t
             .typeText(developerName, data.name)
             .click(firstCheckbox)
             .click('#tried-test-cafe')
             .typeText('#comments', data.comment)
+            .click(interfaceSelector)
+            .click(interFaceOptions.withText('Both'))
             .click('#submit-button')
             .expect(Selector('#article-header').textContent).eql(data.resultText);
     });
