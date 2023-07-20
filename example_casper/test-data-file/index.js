83b5888c5ec2dc5a2ef08be37b9f5bab26605b76
Casporo <casper.g.kaun@gmail.com>
2023-07-20 10:15:53 +0800

    - Added test data file to include OS selection & dropdown - Added checkbox selectors + OS selector - Added lowercase function to convert string for OS selector to lowercase

--- example_casper/test-data-file/index.js
+++ example_casper/test-data-file/index.js
@@ -3,23 +3,31 @@ import XPathSelector from '../xpath-selector';
 
 const dataSet = require('./data.json');
 const firstCheckbox  = XPathSelector('//input[@type="checkbox"]');
+const secondCheckbox = XPathSelector('//input[@type="checkbox"]').nth(1);
 const developerName = XPathSelector('//*[@id="developer-name"]');
 const interfaceSelector = XPathSelector('//*[@id="preferred-interface"]');
 const interFaceOptions = interfaceSelector.find('option');
+const sliderHandle = Selector('#slider').child();
+const rateValue    = Selector('.slider-value').nth(1);
 
 fixture `Data-Driven Tests`
     .page `https://devexpress.github.io/testcafe/example/`;
 
 dataSet.forEach(data => {
     test(`Enter '${data.name}'`, async t => {
+    let result = data.OS.toLowerCase();
+    const OSSelector = Selector('#'+result);
 
         await t
             .typeText(developerName, data.name)
             .click(firstCheckbox)
+            .click(secondCheckbox)
             .click('#tried-test-cafe')
+            .dragToElement(sliderHandle, rateValue, { offsetX: 10,offsetY:10,destinationOffsetX: 0,destinationOffsetY: -25,speed:0.1})
             .typeText('#comments', data.comment)
+            .click(OSSelector)
             .click(interfaceSelector)
-            .click(interFaceOptions.withText('Both'))
+            .click(interFaceOptions.withText(data.dropdown))
             .click('#submit-button')
             .expect(Selector('#article-header').textContent).eql(data.resultText);
     });
